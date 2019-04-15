11787951a25af04c9e1e9c82d3192fdbc26dce15
fredzhang.auf <fredzhang.au@gmail.com>
2019-04-15 11:05:38 +1000

    Add RequireHttpsAttribute class. Modified WebApiConfig, ValuesController.

--- SideSpaceAPI/SideSpaceAPI/App_Start/WebApiConfig.cs
+++ SideSpaceAPI/SideSpaceAPI/App_Start/WebApiConfig.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
 
 namespace SideSpaceAPI
 {
     public static class WebApiConfig
     {
+        public static MySqlConnection conn()
+        {
+            String conn_string = "server=sidespacedbv1.cjvk3kpipmcv.ap-southeast-2.rds.amazonaws.com;port=3306;database=ssdbv1;username=ghost;password=monashie;";
+            MySqlConnection conn = new MySqlConnection(conn_string);
+
+            return conn;
+        }
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -19,6 +28,18 @@ namespace SideSpaceAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "GetApi",
+                routeTemplate: "api/{controller}/{username}"
+            );
+
+            // Web API configuration and services  
+            // config.Filters.Add(new RequireHttpsAttribute());
+
+            var appXmlType = config.Formatters.XmlFormatter
+                .SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
--- SideSpaceAPI/SideSpaceAPI/Controllers/ValuesController.cs
+++ SideSpaceAPI/SideSpaceAPI/Controllers/ValuesController.cs
@@ -4,21 +4,109 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
 
 namespace SideSpaceAPI.Controllers
 {
+    public class Results
+    {
+        public string HName { get; set; }
+        public string Address { get; set; }
+        public string Suburb { get; set; }
+        public string Postcode { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Mental { get; set; }
+        public string Error { get; set; }
+
+        public Results(string HName, string Address, string Suburb, string Postcode, string Lat, string Lon, string Mental, string Error)
+        {
+            this.HName = HName;
+            this.Address = Address;
+            this.Suburb = Suburb;
+            this.Postcode = Postcode;
+            this.Lat = Lat;
+            this.Lon = Lon;
+            this.Mental = Mental;
+            this.Error = Error;
+        }
+    }
+
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<Results> Get()
         {
-            return new string[] { "value1", "value2" };
+            // Create connection
+            MySqlConnection conn = WebApiConfig.conn();
+
+            MySqlCommand query = conn.CreateCommand();
+            query.CommandText = "SELECT * FROM hospital;";
+
+            var results = new List<Results>();
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                results.Add(new Results(null, null, null, null, null, null, null, ex.ToString()));
+            }
+
+            MySqlDataReader fetch_query = query.ExecuteReader();
+
+            while (fetch_query.Read())
+            {
+                results.Add(new Results(fetch_query["HName"].ToString(),
+                    fetch_query["Address"].ToString(),
+                    fetch_query["Suburb"].ToString(),
+                    fetch_query["Postcode"].ToString(),
+                    fetch_query["Lat"].ToString(),
+                    fetch_query["Lon"].ToString(),
+                    fetch_query["Mental"].ToString(),
+                    null));
+            }
+
+            return results;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public List<Results> Get(int ID)
         {
-            return "value";
+            // Create connection
+            MySqlConnection conn = WebApiConfig.conn();
+
+            MySqlCommand query = conn.CreateCommand();
+            query.CommandText = "SELECT * FROM hospital WHERE ID = @ID;";
+
+            query.Parameters.AddWithValue("@ID", ID);
+
+            var results = new List<Results>();
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                results.Add(new Results(null, null, null, null, null, null, null, ex.ToString()));
+            }
+
+            MySqlDataReader fetch_query = query.ExecuteReader();
+
+            while (fetch_query.Read())
+            {
+                results.Add(new Results(fetch_query["HName"].ToString(),
+                    fetch_query["Address"].ToString(),
+                    fetch_query["Suburb"].ToString(),
+                    fetch_query["Postcode"].ToString(),
+                    fetch_query["Lat"].ToString(),
+                    fetch_query["Lon"].ToString(),
+                    fetch_query["Mental"].ToString(),
+                    null));
+            }
+            return results;
         }
 
         // POST api/values
@@ -34,6 +122,46 @@ namespace SideSpaceAPI.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+    }
+
+    public class MentalController : ApiController
+    {
+        // GET api/values
+        public List<Results> Get()
+        {
+            // Create connection
+            MySqlConnection conn = WebApiConfig.conn();
+
+            MySqlCommand query = conn.CreateCommand();
+            query.CommandText = "SELECT * FROM hospital WHERE Mental = 'Y';";
+
+            var results = new List<Results>();
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                results.Add(new Results(null, null, null, null, null, null, null, ex.ToString()));
+            }
+
+            MySqlDataReader fetch_query = query.ExecuteReader();
+
+            while (fetch_query.Read())
+            {
+                results.Add(new Results(fetch_query["HName"].ToString(),
+                    fetch_query["Address"].ToString(),
+                    fetch_query["Suburb"].ToString(),
+                    fetch_query["Postcode"].ToString(),
+                    fetch_query["Lat"].ToString(),
+                    fetch_query["Lon"].ToString(),
+                    fetch_query["Mental"].ToString(),
+                    null));
+            }
+
+            return results;
         }
     }
 }
